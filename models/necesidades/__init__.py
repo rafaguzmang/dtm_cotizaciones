924279ced269b4d845c88215031bf72d387a7111
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-02-08 16:02:41 -0600

    Se agregan indicadores

--- models/necesidades/__init__.py
+++ models/necesidades/__init__.py
@@ -1,4 +1,4 @@
 from . import dtm_client_needs
-# from . import dtm_client_indicadores
-# from . import dtm_client_graph
+from . import dtm_client_indicadores
+from . import dtm_client_graph
 from . import dtm_res_partner
