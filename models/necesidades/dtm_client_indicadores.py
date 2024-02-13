3516d3b2d02863eedcc3bb491b42f2abfe7c4193
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-02-13 15:53:40 -0600

    Merge

--- models/necesidades/dtm_client_indicadores.py
+++ models/necesidades/dtm_client_indicadores.py
@@ -172,9 +172,15 @@ class Indicadores(models.Model):
         # fecha_in = int(self.fecha_inicial.strftime("%j"))
         # fecha_out = int(self.fecha_final.strftime("%j"))
         # fecha = fecha_out - fecha_in
-        uno_porcen = (self.uno_con * 100)/self.uno_days
-        dos_porcen = (self.dos_con * 100)/self.dos_days
-        tres_porcen = (self.tres_con * 100)/self.tres_days
+        uno_porcen = 0
+        dos_porcen = 0
+        tres_porcen = 0
+        if self.uno_days != 0:
+            uno_porcen = (self.uno_con * 100)/self.uno_days
+        if self.dos_days != 0:
+            dos_porcen = (self.dos_con * 100)/self.dos_days
+        if self.tres_days != 0:
+            tres_porcen = (self.tres_con * 100)/self.tres_days
 
         self.env.cr.execute("DELETE FROM dtm_client_graph")
         self.env.cr.execute("INSERT INTO dtm_client_graph (id, nombre, cantidad, porcentaje) VALUES (1, 'Nivel 1 Total', "+str(self.uno_con)+", 100)")
