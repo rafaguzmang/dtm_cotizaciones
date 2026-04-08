166fa9f868ebd0be299386372c95d73be8e9c337
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2026-04-08 12:56:08 -0600

    Se implementan nuevas medidad de seguridad

--- __manifest__.py
+++ __manifest__.py
@@ -4,9 +4,8 @@
     "depends":["mail","contacts","dtm_odt_interna","web"],
     "data":[
         # Security
-        'security/ir.model.access.csv',
         'security/res_groups.xml',
-        'security/model_access.xml',
+        'security/ir.model.access.csv',
 
         # Views
         'views/dtm_client_needs_view.xml',
@@ -21,6 +20,7 @@
         'views/dtm_cotizaciones_recotizacion_view.xml',
         'views/dtm_cotizaciones_encuesta_view.xml',
         'views/dtm_cotizaciones_versiones_view.xml',
+        'views/dtm_necesidades_owl_view.xml',
         # Data
         # 'data/cotizacion_email_template.xml',
 
@@ -30,9 +30,18 @@
     ],
     'assets': {
         'web.assets_backend': [
-            'dtm_cotizaciones/static/src/xml/indicadores.xml',
-            'dtm_cotizaciones/static/src/js/indicadores.js',
+            # CSS
+            'dtm_cotizaciones/static/src/css/necesidades_cliente.css',
             'dtm_cotizaciones/static/src/css/styles.css',
+            'dtm_cotizaciones/static/src/css/necesidades_modal.css',
+            # XML
+            'dtm_cotizaciones/static/src/xml/indicadores.xml',
+            'dtm_cotizaciones/static/src/xml/necesidades_cliente.xml',
+            'dtm_cotizaciones/static/src/xml/modals/necesidades_modal.xml',
+            # JS
+            'dtm_cotizaciones/static/src/js/indicadores.js',
+            'dtm_cotizaciones/static/src/js/necesidades_cliente.js',
+            'dtm_cotizaciones/static/src/js/modals/necesidades_modal.js',
         ],
     },
     'license': 'LGPL-3',
